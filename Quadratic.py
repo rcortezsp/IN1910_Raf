1b4f38e6283f1ac889e8ca74953739010f9e55bf
Rafael <rafaelco@uio.no>
2019-08-29 14:19:04 +0200

    Oppgave 1 b)

--- Quadratic.py
+++ Quadratic.py
@@ -1,9 +1,35 @@
 import numpy as np
+
 class Quadratic:
     def __init__(self,a_2,a_1,a_0):
         self.a_2 = a_2
         self.a_1 = a_1
         self.a_0 = a_0
+
+
 class Quadratic(Quadratic):
     def __call__(self,x):
         return self.a_2 * x**2 + self.a_1 *x + self.a_0
+
+    def __str__(self):
+        if self.a_1 < 0 :
+            fortegn = "-"
+        elif self.a_1 >= 0:
+            fortegn = "+"
+        if self.a_0 < 0 :
+            fortegn_2 = "-"
+        elif self.a_0 >= 0:
+            fortegn_2 = "+"
+        return f"{self.a_2}x^2 {fortegn} {abs(self.a_1)}x {fortegn_2} {abs(self.a_0)}"
+
+def test_Quadratic():
+    f = Quadratic(1, -2, 1)
+    assert abs(f(-1) - 4) < 1e-8
+    assert abs(f(0)  - 1) < 1e-8
+    assert abs(f(1)  - 0) < 1e-8
+
+test_Quadratic()
+"""
+f = Quadratic(1, -2, 1)
+print(f)
+"""
