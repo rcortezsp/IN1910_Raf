1353eb733bfdaadc38ed3e041ea9a1d10a5e43fb
Rafael <rafaelco@uio.no>
2019-08-29 15:25:54 +0200

    Oppgave 1c)

--- Quadratic.py
+++ Quadratic.py
@@ -5,7 +5,7 @@ class Quadratic:
         self.a_2 = a_2
         self.a_1 = a_1
         self.a_0 = a_0
-
+        self.coeffs = self.a_2,self.a_1,self.a_0
 
 class Quadratic(Quadratic):
     def __call__(self,x):
@@ -22,6 +22,12 @@ class Quadratic(Quadratic):
             fortegn_2 = "+"
         return f"{self.a_2}x^2 {fortegn} {abs(self.a_1)}x {fortegn_2} {abs(self.a_0)}"
 
+    def __add__(self,other):
+        a_2 = self.a_2 + other.a_2
+        a_1 = self.a_1 + other.a_1
+        a_0 = self.a_0 + other.a_0
+        return Quadratic(a_2,a_1,a_0)
+
 def test_Quadratic():
     f = Quadratic(1, -2, 1)
     assert abs(f(-1) - 4) < 1e-8
@@ -29,7 +35,16 @@ def test_Quadratic():
     assert abs(f(1)  - 0) < 1e-8
 
 test_Quadratic()
-"""
+
 f = Quadratic(1, -2, 1)
-print(f)
-"""
+
+def test_Quadratic_add():
+    f = Quadratic(1, -2, 1)
+    g = Quadratic(-1, 6, -3)
+    h = f + g
+    a2, a1, a0 = h.coeffs
+    assert a2 == 0
+    assert a1 == 4
+    assert a0 == -2
+
+test_Quadratic_add()
